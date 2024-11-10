25bdf92f8181242fb74754cfe6c793d8bf9932da
jhimi01 <talukderjhimi@gmail.com>
2024-11-10 21:01:14 +0600

    fixed data after refreash

--- src/contexts/AccountContext.tsx
+++ src/contexts/AccountContext.tsx
@@ -49,14 +49,6 @@ export const AccountProvider = ({
     setExpenses(savedExpenses);
   }, []);
 
-  useEffect(() => {
-    localStorage.setItem("income", JSON.stringify(income));
-  }, [income]);
-
-  useEffect(() => {
-    localStorage.setItem("expenses", JSON.stringify(expenses));
-  }, [expenses]);
-
   return (
     <AccountContext.Provider
       value={{ income, setIncome, expenses, setExpenses }}
